c118546f27f1daf4847787c3e5d34143e6bba155
amazonooo <garikogannisyan.9@gmail.com>
2025-01-20 22:08:47 +0300

    page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,26 @@
+import { Button } from '@/components/ui/buttons/Button'
+import { COLORS } from '@/constants/color.constants'
+import { GanttChartSquare } from 'lucide-react'
+import Link from 'next/link'
+
 export default function Home() {
-  return (
-    <div></div>
-  )
+	return (
+		<div className="min-h-screen flex items-center justify-center flex-col gap-y-4">
+			<div className="flex items-center">
+				<h1 className="text-3xl font-bold">Welcome to</h1>
+				<Link href="/" className="flex items-center gap-2.5 p-layout">
+					<GanttChartSquare color={COLORS.primary} size={38} />
+					<span className="text-3xl font-bold relative">
+						AMA Planner
+						{/* <span className="absolute -top-1.5 -right-6 text-xs opacity-40 rotate-[18deg] font-normal">
+							beta
+						</span> */}
+					</span>
+				</Link>
+			</div>
+			<Link href={'/i'}>
+				<Button>Dashboard</Button>
+			</Link>
+		</div>
+	)
 }
